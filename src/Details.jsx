e4712e5db443e1fba276f793dcd833b152271172
jalbiston <jacob.albiston@gmail.com>
2021-10-28 11:00:17 -0600

    shopping game

---
+++ src/Details.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+import { useParams } from "react-router-dom"
+import useFetch from './services/useFetch'
+import Spinner from "./Spinner"
+
+export default function Details(){
+    const { id } = useParams()
+    const { data: product, loading, error} = useFetch(`products/${id}`)
+    
+    if (loading) return <Spinner />
+    if (error) throw error;
+        return (
+            <div id="detail">
+                <h1>{product.name}</h1>
+                <p>{product.description}</p>
+                <p id="price">${product.price}</p>
+                <img src={`/images/${product.image}`} alt={product.category} />
+            </div>
+        )
+}
